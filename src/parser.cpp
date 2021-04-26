5906af72ec0e4b346716103c1ead118b8e9e6002
understreck <pqfrml@gmail.com>
2021-04-26 12:29:14 +0200

    Move metamagic into a header

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,6 @@
 #include "parser.hpp"
 
+#include "meta.hpp"
 #include "token.hpp"
 #include "ast.hpp"
 
@@ -10,35 +11,8 @@ namespace elderLISP {
 
 namespace parser {
 
-    template<class T, class... Us>
-    struct Index {};
-
-    template<class T, class... Us>
-    struct Index<T, T, Us...> {
-        static auto constexpr value = 0ul;
-    };
-
-    template<class T, class U, class... Vs>
-    struct Index<T, U, Vs...> {
-        static auto constexpr value = 1ul + Index<T, Vs...>::value;
-    };
-
-    template<class T, class... Us>
-    auto constexpr index = Index<T, Us...>::value;
-
-    template<class T, class U>
-    struct VariantIndex {};
-
-    template<class T, class... Us>
-    struct VariantIndex<T, std::variant<Us...>> {
-        static auto constexpr value = index<T, Us...>;
-    };
-
-    template<class T, class Variant>
-    auto constexpr variantIndex = VariantIndex<T, Variant>::value;
-
     template<class T>
-    auto constexpr tokenIndex = variantIndex<T, token::Token>;
+    auto constexpr tokenIndex = meta::variantIndex<T, token::Token>;
 
     using TokenIt = std::deque<token::Token>::const_iterator;
 
