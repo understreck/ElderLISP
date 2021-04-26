cd0e888acbd62eea6cc8f5cbeb048c73f67bee14
understreck <pqfrml@gmail.com>
2021-04-26 12:21:21 +0200

    WIP: interpreter

--- include/interpreter.hpp
+++ include/interpreter.hpp
@@ -3,11 +3,20 @@
 
 #include "ast.hpp"
 
+#include <deque>
+#include <string>
+#include <unordered_map>
+
 namespace elderLISP {
 
-auto
-interpret(ast::List program, ast::List memory = {}) -> ast::List;
+namespace interpreter {
+    using Environment = std::deque<std::unordered_map<
+            std::string,
+            std::variant<ast::List, ast::Atom>>>;
 
+    auto
+    interpret(ast::List program, Environment environment) -> Environment;
+}    // namespace interpreter
 }    // namespace elderLISP
 
 #endif    // ELDERLISP_INTERPRETER_HPP
--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -2,38 +2,16 @@
 #include "ast.hpp"
 
 namespace elderLISP {
+namespace interpreter {
 
-// auto
-// interpret(ast::List)
 
-//auto
-//interpret(ast::List const& program, token::Token token, Memory memory) -> Memory
-//{
-    //auto const interp = [&, memory = std::move(memory)](auto&& cell) -> Memory {
-        //return interpret(program, cell, memory);
-    //};
 
-    //return std::visit(interp, token);
-//};
+    auto
+    interpret(ast::List program, Environment environment) -> Environment
+    {
+        environment.push_back({});
 
-//auto
-//interpret(ast::List const& program, ast::List const cell, Memory memory)
-        //-> Memory
-//{
-    //memory = push(std::move(memory));
-
-    //memory = 
-
-    //if(notGlobal) {
-        //memory = drop(std::move(memory));
-    //}
-    //return memory;
-//};
-
-//auto
-//interpret(ast::List program, ast::List memory) -> ast::List
-//{
-    //return interpret(program, 0, std::move(memory));
-//}
-
+        return std::visit(interpreter, program;
+    }
+}    // namespace interpreter
 }    // namespace elderLISP
