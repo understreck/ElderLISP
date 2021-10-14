b6010b6a7e3df2cdf7ea0da0f525d7bfd3373342
understreck <pqfrml@gmail.com>
2021-10-14 17:01:20 +0200

    Finnished simple environment

--- include/environment.hpp
+++ include/environment.hpp
@@ -1,27 +1,81 @@
 #ifndef ELDERLISTP_ENVIRONMENT_HPP
 #define ELDERLISTP_ENVIRONMENT_HPP
 
+#include <string_view>
+#include <tuple>
+
 #include "list.hpp"
 
-template<atom_or_list Key, atom_or_list Value>
-using KeyValuePair = List<Key, Value>;
+template<atom_or_list Value>
+struct KeyValuePair {
+    std::string_view key;
+    Value value;
+};
+
+template<atom_or_list T>
+KeyValuePair(std::string_view, T) -> KeyValuePair<T>;
 
 template<class... Ts>
 concept key_value_pair = (isSpecalisationOf<Ts, KeyValuePair> && ...);
 
 template<class...>
-class Environment {};
+struct Environment {};
 
 template<key_value_pair... KVPs>
 struct Environment<KVPs...> {
-    List<KVPs...> kvps;
+    std::tuple<KVPs...> kvps;
 };
+
+template<key_value_pair... KVPs>
+Environment(KVPs...) -> Environment<KVPs...>;
+
+template<key_value_pair... KVPs>
+Environment(std::tuple<KVPs...>) -> Environment<KVPs...>;
 
 template<class Env, key_value_pair... KVPs>
 requires isSpecalisationOf<Env, Environment>
 struct Environment<Env, KVPs...> {
-    List<KVPs...> kvps;
+    std::tuple<KVPs...> kvps;
     Env outerEnvironment;
 };
 
+template<class T>
+concept environment = isSpecalisationOf<T, Environment>;
+
+template<environment Env, key_value_pair... KVPs>
+Environment(std::tuple<KVPs...>, Env) -> Environment<Env, KVPs...>;
+
+template<class Env>
+concept has_outer_env = requires(Env env)
+{
+    env.outerEnvironment;
+};
+
+auto constexpr push_kvp(environment auto env, key_value_pair auto kvp)
+{
+    return Environment{std::tuple_cat(std::tuple{kvp}, env.kvps)};
+}
+
+template<environment Env>
+requires has_outer_env<Env>
+auto constexpr push_kvp(Env env, key_value_pair auto kvp)
+{
+    return Environment{
+            std::tuple_cat(std::tuple{kvp}, env.kvps),
+            env.outerEnvironment};
+}
+
+template<environment Env>
+auto constexpr push_scope(Env outer)
+{
+    return Environment{{}, outer};
+}
+
+template<environment Env>
+auto constexpr pop_scope(Env inner)
+{
+    static_assert(has_outer_env<Env>, "Needs an outerEnvironment");
+
+    return inner.outerEnvironment;
+}
 #endif    // ELDERLISTP_ENVIRONMENT_HPP
