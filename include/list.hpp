4b6f4ee3421a4c3e65612dc5c45d1362947772d9
understreck <pqfrml@gmail.com>
2021-10-17 15:29:08 +0200

    Make all data types into integral constants so values and types are intertwined

--- include/list.hpp
+++ include/list.hpp
@@ -1,11 +1,13 @@
 #ifndef ELDERLISTP_LIST_HPP
 #define ELDERLISTP_LIST_HPP
 
-#include <variant>
-#include <tuple>
+#include <cstddef>
 #include <string_view>
+#include <tuple>
+#include <variant>
 
-enum class CoreInstruction {
+// CoreInstruction
+enum CoreInstruction_enum {
     LAMBDA,
     DEFINE,
     QUOTE,
@@ -17,6 +19,52 @@ enum class CoreInstruction {
     CONDITION
 };
 
+template<CoreInstruction_enum ci>
+struct CoreInstruction : std::integral_constant<CoreInstruction_enum, ci> {};
+
+template<class T>
+concept core_instruction = std::is_same_v<T, CoreInstruction<T::value>>;
+
+template<CoreInstruction_enum ci>
+auto constexpr CI = CoreInstruction<ci>{};
+
+// Char
+template<char c>
+struct Character : std::integral_constant<char, c> {};
+
+template<class T>
+concept character = std::is_same_v<T, Character<T::value>>;
+
+template<char c>
+auto constexpr Char = Character<c>{};
+
+// Label
+template<char... cs>
+struct Label : std::tuple<Character<cs>...> {};
+
+template<char... cs>
+auto constexpr Lbl = Label<cs...>{};
+
+template<class T>
+concept label = std::is_same_v<T, Label<T::value>>;
+
+// Integer
+template<int i>
+struct Integer : std::integral_constant<int, i> {};
+
+template<class T>
+concept integer = std::is_same_v<T, Integer<T::value>>;
+
+template<int i>
+auto constexpr Int = Integer<i>{};
+
+//template<class T, class... Us>
+//auto constexpr isSameAsOneOf = (std::is_same_v<T, Us> || ...);
+
+template<class T>
+concept data_type =
+        core_instruction<T> || character<T> || label<T> || integer<T>;
+
 template<class T, template<class...> class U>
 struct IsSpecalisationOf : std::false_type {};
 
@@ -24,19 +72,24 @@ template<template<class...> class T, class... Args>
 struct IsSpecalisationOf<T<Args...>, T> : std::true_type {};
 
 template<class T, template<class...> class U>
-auto constexpr isSpecalisationOf = IsSpecalisationOf<T, U>::value;
-
-struct Atom : std::variant<CoreInstruction, bool, int> {};
+concept is_specialisation_of = IsSpecalisationOf<T, U>::value;
 
 template<class...>
 struct List;
 
-template<class... Ts>
-concept atom_or_list =
-        ((std::is_same_v<Ts, Atom> || isSpecalisationOf<Ts, List>)&&...);
-
 template<>
 struct List<> : std::tuple<> {};
+
+auto constexpr NIL = List<>{};
+
+template<class T>
+concept atom = std::is_same_v<T, List<>> || data_type<T>;
+
+template<class T>
+concept list = is_specialisation_of<T, List>;
+
+template<class... Ts>
+concept atom_or_list = ((atom<Ts> || list<Ts>)&&...);
 
 template<atom_or_list T>
 struct List<T> : std::tuple<T> {};
@@ -56,53 +109,5 @@ struct List<T, U, Us...> : std::tuple<T, List<U, Us...>> {
 
 template<atom_or_list... Ts>
 List(Ts...) -> List<Ts...>;
-
-auto constexpr first_impl(atom_or_list auto)
-{
-    return List{};
-}
-
-template<class T, class... Us>
-auto constexpr first_impl(List<T, Us...> l)
-{
-    return std::get<0>(l);
-}
-
-auto constexpr first = [](atom_or_list auto l) {
-    return first_impl(l);
-};
-
-auto constexpr rest_impl(atom_or_list auto)
-{
-    return List{};
-}
-
-template<class T, class... Us>
-auto constexpr rest_impl(List<T, Us...> l)
-{
-    return std::get<1>(l);
-}
-
-auto constexpr rest = [](atom_or_list auto l) {
-    return rest_impl(l);
-};
-
-auto constexpr cons = []<atom_or_list LHS, atom_or_list RHS>(LHS lhs, RHS rhs) {
-    if constexpr(std::is_same_v<LHS, List<>>) {
-        return rhs;
-    }
-
-    if constexpr(std::is_same_v<RHS, List<>>) {
-        return lhs;
-    }
-
-    return List{lhs, rhs};
-};
-
-template<atom_or_list LHS, atom_or_list RHS>
-requires(!std::is_same_v<LHS, RHS>) auto constexpr operator==(LHS, RHS)
-{
-    return false;
-}
 
 #endif    // ELDERLISTP_LIST_HPP
