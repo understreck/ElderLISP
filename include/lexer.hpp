0b9bf98eb0d7d72c3c1f090f437e8e5bb9d8d9a3
understreck <pqfrml@gmail.com>
2021-10-23 16:44:00 +0200

    Need to actually use pointer arithmetic for a part of the program

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -10,19 +10,23 @@
 
 #include "list.hpp"
 
-auto constexpr matches = std::tuple{ctre::match<"\\s*\\(">,
-ctre::match<"\\s*lambda">,
-ctre::match<"\\s*define">,
-ctre::match<"\\s*quote">,
-ctre::match<"\\s*atom">,
-ctre::match<"\\s*=">,
-ctre::match<"\\s*car">,
-ctre::match<"\\s*cdr">,
-ctre::match<"\\s*cons">,
-ctre::match<"\\s*if">,
-ctre::match<"\\s*list">,
-ctre::match<"\\s*\\*">,
-ctre::match<"\\s*-">,
+auto constexpr matches = std::tuple{
+        ctre::match<"\\s*\\(">,
+        ctre::match<"\\s*lambda">,
+        ctre::match<"\\s*define">,
+        ctre::match<"\\s*quote">,
+        ctre::match<"\\s*atom">,
+        ctre::match<"\\s*=">,
+        ctre::match<"\\s*car">,
+        ctre::match<"\\s*cdr">,
+        ctre::match<"\\s*cons">,
+        ctre::match<"\\s*if">,
+        ctre::match<"\\s*list">,
+        ctre::match<"\\s*\\*">,
+        ctre::match<"\\s*-">,
+        ctre::match<"\\s*\\+">,
+        ctre::match<"\\s*\\/">,
+        ctre::match<"\\s*%">,
 };
 
 template<ctll::fixed_string m_string>
@@ -40,10 +44,10 @@ struct IsLineT<LineT<s>> : std::true_type {};
 template<class T>
 concept c_line = IsLineT<T>::value;
 
-//template<size_t position, class... Elements>
-//auto consteval parse(c_line auto line, std::tuple<Elements...> elements)
-//{}
+template<size_t position, class... Elements>
+auto consteval parse(c_line auto line, std::tuple<Elements...> elements)
+{}
 
-//auto consteval parse(c_line auto line)
-//{}
+auto consteval parse(c_line auto line)
+{}
 #endif    // ELDER_LISP_LEXER_HPP
