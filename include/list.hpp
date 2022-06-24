e89d571e752549525b563c8bc2d8090e64283695
understreck <pqfrml@gmail.com>
2022-06-24 17:48:39 +0200

    Created the barebones fundamental types

--- include/list.hpp
+++ include/list.hpp
@@ -3,6 +3,7 @@
 
 #include <deque>
 #include <variant>
+#include <string>
 
 /* Stuff that needs to be implementet
  * quote/"
@@ -17,4 +18,42 @@
  * if
  */
 
-#endif //ELDERLISP_LIST_HPP
+namespace elder {
+struct Data {};    // placeholder type for actual data
+struct Symbol {
+    std::string name;
+
+    auto constexpr
+    operator*() const noexcept -> std::string const&
+    {
+        return name;
+    }
+};
+struct NIL {};    // Signifies end of list
+
+enum class Builtin {
+    QUOTE,
+    LIST,
+    CAR,
+    CDR,
+    CONS,
+    ATOM,
+    EQ,
+    DEF,
+    LAMBDA,
+    IF,
+
+    FRONT = CAR,
+    BACK  = CDR,
+    JOIN  = CONS,
+    COMP  = EQ
+};
+
+struct Element : std::variant<NIL, Data, Builtin> {};
+struct List;
+using return_t = std::variant<List, Element>;
+struct List : std::deque<return_t> {};
+
+}    // namespace elder
+
+#endif    // ELDERLISP_LIST_HPP
