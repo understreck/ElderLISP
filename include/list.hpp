717c65a3ad5f0103b8d2dac3a85d424fd8546f7a
understreck <pqfrml@gmail.com>
2022-06-24 10:19:30 +0200

    added list of core ops

---
+++ include/list.hpp
@@ -0,0 +1,20 @@
+#ifndef ELDERLISP_LIST_HPP
+#define ELDERLISP_LIST_HPP
+
+#include <deque>
+#include <variant>
+
+/* Stuff that needs to be implementet
+ * quote/"
+ * list
+ * car/front
+ * cdr/back
+ * cons/join
+ * atom
+ * eq/comp
+ * def
+ * lambda
+ * if
+ */
+
+#endif //ELDERLISP_LIST_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "list.hpp"
 
 auto
 main() -> int
